f70bb0038b151c83f429ad950fd0d81483fc75ce
arionitmisini <arionitmisini@gmail.com>
2018-06-25 14:56:34 +0200

    Create React App

--- src/main/java/com/arioniti/expenxemanager/expensemanager/controller/ExpenseController.java
+++ src/main/java/com/arioniti/expenxemanager/expensemanager/controller/ExpenseController.java
@@ -5,10 +5,9 @@ import com.arioniti.expenxemanager.expensemanager.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,6 +22,30 @@ public class ExpenseController {
         List<Expense> result = expenseService.findAll();
 
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/{year}/{month}")
+    public ResponseEntity<?> getByMonthAndYear(@PathVariable("year") int year, @PathVariable("month") String month) {
+        List<Expense> result = new ArrayList<>();
+        if ("All".equals(month)) {
+            result = expenseService.findByYear(year);
+        } else {
+            result = expenseService.findByMonthAndYear(month, year);
+        }
+        return new ResponseEntity(result, HttpStatus.OK);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addOrUpdateExpense(@RequestBody Expense expense) {
+        expenseService.saveOrUpdateExpense(expense);
+
+        return new ResponseEntity("Expense added succcessfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public void deleteExpense(@RequestParam("id") String id) {
+        expenseService.deleteExpense(id);
     }
 
 
